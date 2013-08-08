61c79eba02171e6dcf744c98a778119af36a8585
Austin Pocus <ajpocus@gmail.com>
2013-08-08 17:22:10 +0000

    basic init

---
+++ index.js
@@ -0,0 +1,19 @@
+var MongoClient = require('mongodb').MongoClient,
+  util = require('util'),
+  stream = require('stream');
+
+function MongoStream(opts) {
+  stream.Writable.call(this, opts);
+  
+  if (!opts) { opts = {}; }
+  this._max = opts.max || 1000000;
+  this._db = opts.databaseUri || "mongodb://localhost/mongostream";
+  this._collection = opts.collection || "mongostream";
+}
+util.inherits(MongoStream, stream.Writable);
+
+MongoStream.prototype._write = function (chunk, encoding, cb) {
+  
+};
+
+exports = module.exports = MongoStream;
