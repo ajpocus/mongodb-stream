d725196f3547cb519bf3fd8819046af7cec61701
Austin Pocus <ajpocus@gmail.com>
2013-08-08 17:23:47 +0000

    one hand washes the other; no max without idx

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function MongoStream(opts) {
   stream.Writable.call(this, opts);
   
   if (!opts) { opts = {}; }
+  this._index = 1;
   this._max = opts.max || 1000000;
   this._db = opts.databaseUri || "mongodb://localhost/mongostream";
   this._collection = opts.collection || "mongostream";
