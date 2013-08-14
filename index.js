02c6f1b9ae7158c6809bf6296bfe88e2ffcdf8f5
Austin Pocus <ajpocus@gmail.com>
2013-08-14 11:42:55 -0700

    fix fencepost error

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ MongoStream.prototype._write = function (chunk, encoding, cb) {
     if (err) { return cb(err); }
     
     self._index += 1;
-    if (self._index >= self._max) { self.end(); }
+    if (self._index > self._max) { self.end(); }
     return cb(null);
   });
 };
